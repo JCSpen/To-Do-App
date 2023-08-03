e647def21426c85937ecc277d6b0c1f1d1192456
JCSpen <103535034+JCSpen@users.noreply.github.com>
2023-08-04 00:19:19 +0100

    Added Complete and Exit Buttons

--- GUI.py
+++ GUI.py
@@ -5,10 +5,12 @@ label = p.Text("Type in a To-Do")
 input_box = p.InputText(tooltip="Enter a to do", key='todo')
 add_button = p.Button("Add")
 edit_button = p.Button("Edit")
+complete_button = p.Button("Complete")
+exit_button = p.Button("Exit")
 list_box = p.Listbox(values=f.get_todos(), key='todos',
                      enable_events=True, size=[45, 10])
 window = p.Window('To-Do-App',
-                  layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                  layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]],
                   font=('Helvetica', 20))
 
 while True:
@@ -28,6 +30,17 @@ while True:
             todos[index] = new_todo + "\n"
             f.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = f.get_todos()
+            todos.remove(todo_to_complete)
+            f.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case p.WIN_CLOSED:
             break
 window.close()
